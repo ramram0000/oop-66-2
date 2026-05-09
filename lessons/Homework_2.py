b00c01a9df0ea0dd4c3380c639b19b9adb5c2ff8
ramilramil555 <syrgakovramil2010@gmail.com>
2026-05-09 20:37:38 +0600

    added Homework_2

---
+++ lessons/Homework_2.py
@@ -0,0 +1,76 @@
+import random
+
+class Hero:
+    def _init_(self, name, level, health, strength):
+        self.name = name
+        self.level = level
+        self.health = health
+        self.strength = strength
+
+    def greet(self):
+        print(f"Привет, я {self.name}, мой уровень {self.level}")
+
+    def attack(self):
+        print(f"{self.name} атакует!")
+
+    def rest(self):
+        self.health += 10
+        print(f"{self.name} отдыхает и восстанавливает здоровье до {self.health}")
+
+class Warrior(Hero):
+    def _init_(self, name, level, health, strength, stamina):
+        super()._init_(name, level, health, strength)
+        self.stamina = stamina
+
+    def attack(self):
+        print("Воин атакует мечом!")
+
+
+class Mage(Hero):
+    def _init_(self, name, level, health, strength, mana):
+        super()._init_(name, level, health, strength)
+        self.mana = mana
+
+    def attack(self):
+        print("Маг кастует заклинание!")
+
+
+class Assassin(Hero):
+    def _init_(self, name, level, health, strength, stealth):
+        super()._init_(name, level, health, strength)
+        self.stealth = stealth
+
+    def attack(self):
+        print("Ассасин атакует из-под тишка!")
+
+warrior = Warrior("Воин", 1, 100, 20, 50)
+mage = Mage("Маг", 1, 80, 25, 100)
+assassin = Assassin("Ассасин", 1, 90, 30, 80)
+
+heroes = {
+    "warrior": warrior,
+    "mage": mage,
+    "assassin": assassin
+}
+
+choice = input("Выберите героя: Warrior / Mage / Assassin\n").lower()
+
+if choice not in heroes:
+    print("Неверный выбор!")
+else:
+    player = heroes[choice]
+    enemy = random.choice(list(heroes.values()))
+
+    print(f"\nВы выбрали: {player.name}")
+    print(f"Противник: {enemy.name}\n")
+
+    if player == enemy:
+        print("Ничья!")
+    elif (
+        (isinstance(player, Warrior) and isinstance(enemy, Assassin)) or
+        (isinstance(player, Assassin) and isinstance(enemy, Mage)) or
+        (isinstance(player, Mage) and isinstance(enemy, Warrior))
+    ):
+        print(f"{player.name} победил!")
+    else:
+        print(f"{enemy.name} победил!")
---
+++ lessons/lesson_2.py
@@ -0,0 +1,7 @@
+class Hero:
+    def __init__(self, name, hp, lvl):
+        self.name = name
+        self.hp = hp
+        self.lvl = lvl
+
+        def action
